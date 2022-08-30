63f88c1ee8683eeb6b5836eb23faaae3898d92ba
Ashwin A Nayar <ashwin5059198@gmail.com>
2022-08-30 23:25:11 +0530

    A1Q3 complete

---
+++ Assignment-01/A01_Q03_NA19B001.cpp
@@ -0,0 +1,43 @@
+/**
+ * @file     A01_Q03_NA19B001.cpp
+ * @author   Ashwin A Nayar (na19b001@smail.iitm.ac.in)
+ */
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+    int n1, n2;
+    cin >> n1 >> n2;
+
+    vector<int> arr1(n1), arr2(n2);
+    for (auto& e : arr1) cin >> e;
+    for (auto& e : arr2) cin >> e;
+
+    int i = 0, j = 0;
+    int mid1, mid2;
+
+    for (int k = 0; k <= (n1 + n2)/2; ++k) {
+        mid2 = mid1;
+        if (i != n1 && j != n2) {
+            mid1 = arr1[i] < arr2[j] ? arr1[i++] : arr2[j++];
+        } else if (i < n1) {
+            mid1 = arr1[i++];
+        } else {
+            mid1 = arr2[j++];
+        }
+    }
+
+    double median;
+    if ((n1 + n2)&1) {
+        median = mid1;
+    } else {
+        median = (double)(mid1 + mid2)/2.0;
+    }
+
+    printf("%.2lf\n", median);
+
+    return EXIT_SUCCESS;
+}
